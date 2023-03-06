c19d645e3b606a3b6739bb87c2fbd5fadd825240
antost360 <antost360@gmail.com>
2023-03-06 11:20:56 +0100

    working version, client in progress

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const cors = require("cors")
 const mysql = require("mysql")
-const app = express()
 const port = 3000
-
+const app = express()
 app.use(cors())
 
-//tworzenie połączenie obsługa błędów err = bool
+app.get("/", (req, res) => {
+    res.send("ok")
+})
+
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -14,18 +16,9 @@ var con = mysql.createConnection({
     database: "0603"
 })
 
-con.connect(function (err) {
+con.connect((err) => {
     if (err) console.log(err)
-    else console.log("połączono z bazą danych")
-})
-
-//fields = info o result
-app.get("/select", (req, res) => {
-    const sql = "SELECT * FROM uczniowie"
-    con.query(sql, (err, result, fields) => {
-        if (err) console.log(err)
-        else res.send(result)
-    })
+    else console.log("polączono z bazą danych")
 })
 
 app.get("/add/:imie/:nazwisko/:klasa", (req, res) => {
@@ -33,23 +26,25 @@ app.get("/add/:imie/:nazwisko/:klasa", (req, res) => {
     const nazwisko = req.params.nazwisko
     const klasa = req.params.klasa
 
-    const sql = `INSERT INTO uczniowie (imie, nazwisko, klasa) VALUES ('${imie}', '${nazwisko}', '${klasa}')`
+    const sql = `INSERT INTO uczniowie (imie, nazwisko, klasa) VALUES ('${imie}', '${nazwisko}','${klasa}')`
 
     con.query(sql, (err, result, fields) => {
         if (err) console.log(err)
         else res.send("dodano rekord")
     })
+})
 
+app.get("/select", (req, res) => {
+    const sql = "SELECT * FROM uczniowie"
+
+    con.query(sql, (err, result, fields) => {
+        if (err) console.log(err)
+        else res.send(result)
+    })
 })
 
 
 
-
-
-
-app.get("/", (req, res) => {
-    res.send("ok")
-})
 app.listen(port, () => {
-    console.log("app works on port: " + port)
+    console.log(`app works on port: ${port}`)
 })
