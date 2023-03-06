d668cb7df1fa96f9804d86b121624203f9876ae4
antost360 <antost360@gmail.com>
2023-03-06 10:09:32 +0100

    sql works on endpoint

--- server/index.js
+++ server/index.js
@@ -6,23 +6,27 @@ const port = 3000
 
 app.use(cors())
 
+//tworzenie połączenie obsługa błędów err = bool
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    databse: "06.03"
+    database: "0603"
 })
 
 con.connect(function (err) {
     if (err) console.log(err)
     else console.log("połączono z bazą danych")
-
-
-
 })
 
-
-
+//fields = info o result
+app.get("/select", (req, res) => {
+    const sql = "SELECT * FROM uczniowie"
+    con.query(sql, (err, result, fields) => {
+        if (err) console.log(err)
+        else res.send(result)
+    })
+})
 
 
 app.get("/", (req, res) => {
