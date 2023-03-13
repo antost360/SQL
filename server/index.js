9f59cf91f32fb15d9ee6d4412a494f796393ffa7
antost360 <antost360@gmail.com>
2023-03-13 16:37:25 +0100

    end game

--- server/index.js
+++ server/index.js
@@ -3,23 +3,24 @@ const cors = require("cors")
 const app = express()
 const mysql = require("mysql")
 const port = 3000
-
+const database = "1003"
+const table = "uczniowie"
 app.use(cors())
 
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "1003",
+  database: `${database}`,
 })
 
 con.connect((err) => {
   if (err) console.log(err)
-  else console.log("connected to database")
+  else console.log(`connected to "${database}"`)
 })
 
 app.get("/itemlist", (req, res) => {
-  const sql = "SELECT * FROM uczniowie"
+  const sql = `SELECT * FROM ${table}`
 
   con.query(sql, (err, result, fields) => {
     if (err) console.log(err)
@@ -31,7 +32,7 @@ app.get("/additem/:imie/:nazwisko/:klasa", (req, res) => {
   const imie = req.params.imie
   const nazwisko = req.params.nazwisko
   const klasa = req.params.klasa
-  const sql = `INSERT INTO uczniowie (imie, nazwisko, klasa) VALUES ('${imie}', '${nazwisko}', '${klasa}')`
+  const sql = `INSERT INTO ${table} (imie, nazwisko, klasa) VALUES ('${imie}', '${nazwisko}', '${klasa}')`
 
   con.query(sql, (err, result, fields) => {
     if (err) console.log(err)
@@ -41,7 +42,7 @@ app.get("/additem/:imie/:nazwisko/:klasa", (req, res) => {
 
 app.get("/usun/:data", (req, res) => {
   const data = req.params.data
-  const sql = `DELETE FROM uczniowie WHERE id=${data};`
+  const sql = `DELETE FROM ${table} WHERE id=${data};`
 
   con.query(sql, (err, result, fields) => {
     if (err) console.log(err)
